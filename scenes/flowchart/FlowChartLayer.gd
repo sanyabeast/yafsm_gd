543adc7eed5f0b0de96cc0cd4f3bacf2d2ed52f1
@sanyabeast <a.gvrnsk@gmail.com>
2025-11-25 03:48:16 +0200

    Add type hints and minor refactoring to FlowChartLayer
    Added explicit type hints to variables and function signatures for improved code clarity and static analysis. Minor refactoring includes formatting adjustments and variable initializations. No functional changes were made.

--- scenes/flowchart/FlowChartLayer.gd
+++ scenes/flowchart/FlowChartLayer.gd
@@ -1,106 +1,107 @@
 @tool
 extends Control
+
+
 const FlowChartNode = preload("res://addons/yafsm/scenes/flowchart/FlowChartNode.gd")
 
-var content_lines = Control.new() # Node that hold all flowchart lines
-var content_nodes = Control.new() # Node that hold all flowchart nodes
+var content_lines: Control = Control.new()
+var content_nodes: Control = Control.new()
+var _connections: Dictionary = {}
 
-var _connections = {}
 
-func _init():
-	
+func _init() -> void:
 	name = "FlowChartLayer"
 	mouse_filter = MOUSE_FILTER_IGNORE
 
 	content_lines.name = "content_lines"
 	content_lines.mouse_filter = MOUSE_FILTER_IGNORE
 	add_child(content_lines)
-	move_child(content_lines, 0) # Make sure content_lines always behind nodes
+	move_child(content_lines, 0)
 
 	content_nodes.name = "content_nodes"
 	content_nodes.mouse_filter = MOUSE_FILTER_IGNORE
 	add_child(content_nodes)
 
-func hide_content():
+
+func hide_content() -> void:
 	content_nodes.hide()
 	content_lines.hide()
 
-func show_content():
+
+func show_content() -> void:
 	content_nodes.show()
 	content_lines.show()
 
-# Get required scroll rect base on content
-func get_scroll_rect(scroll_margin=0):
-	var rect = Rect2()
+
+func get_scroll_rect(scroll_margin: float = 0.0) -> Rect2:
+	var rect: Rect2 = Rect2()
 	for child in content_nodes.get_children():
-		# Every child is a state/statemachine node
-		var child_rect = child.get_rect()
+		var child_rect: Rect2 = child.get_rect()
 		rect = rect.merge(child_rect)
 	return rect.grow(scroll_margin)
 
-# Add node
-func add_node(node):
+
+func add_node(node: Node) -> void:
 	content_nodes.add_child(node)
 
-# Remove node
-func remove_node(node):
+
+func remove_node(node: Node) -> void:
 	if node:
 		content_nodes.remove_child(node)
 
-# Called after connection established
-func _connect_node(connection):
+
+func _connect_node(connection: Connection) -> void:
 	content_lines.add_child(connection.line)
 	connection.join()
 
-# Called after connection broken
-func _disconnect_node(connection):
+
+func _disconnect_node(connection: Connection) -> Control:
 	content_lines.remove_child(connection.line)
 	return connection.line
 
-# Rename node
-func rename_node(old, new):
+
+func rename_node(old: String, new: String) -> void:
 	for from in _connections.keys():
-		if from == old: # Connection from
-			var from_connections = _connections[from]
+		if from == old:
+			var from_connections: Dictionary = _connections[from]
 			_connections.erase(old)
 			_connections[new] = from_connections
-		else: # Connection to
+		else:
 			for to in _connections[from].keys():
 				if to == old:
-					var from_connection = _connections[from]
-					var value = from_connection[old]
+					var from_connection: Dictionary = _connections[from]
+					var value: Connection = from_connection[old]
 					from_connection.erase(old)
 					from_connection[new] = value
 
-# Connect two nodes with a line
-func connect_node(line, from, to, interconnection_offset=0):
+
+func connect_node(line: Control, from: String, to: String, interconnection_offset: int = 0) -> void:
 	if from == to:
-		return # Connect to self
-	var connections_from = _connections.get(from)
+		return
+	var connections_from: Variant = _connections.get(from)
 	if connections_from:
 		if to in connections_from:
-			return # Connection existed
-	var connection = Connection.new(line, content_nodes.get_node(NodePath(from)), content_nodes.get_node(NodePath(to)))
+			return
+	var connection: Connection = Connection.new(line, content_nodes.get_node(NodePath(from)), content_nodes.get_node(NodePath(to)))
 	if connections_from == null:
 		connections_from = {}
 		_connections[from] = connections_from
 	connections_from[to] = connection
 	_connect_node(connection)
 
-	# Check if connection in both ways
 	connections_from = _connections.get(to)
 	if connections_from:
-		var inv_connection = connections_from.get(from)
+		var inv_connection: Variant = connections_from.get(from)
 		if inv_connection:
 			connection.offset = interconnection_offset
 			inv_connection.offset = interconnection_offset
 			connection.join()
 			inv_connection.join()
 
-# Break a connection between two node
-func disconnect_node(from, to):
-	var connections_from = _connections.get(from)
-	var connection = connections_from.get(to)
+
+func disconnect_node(from: String, to: String) -> Control:
+	var connections_from: Variant = _connections.get(from)
+	var connection: Variant = connections_from.get(to)
 	if connection == null:
 		return
 
@@ -112,46 +113,48 @@ func disconnect_node(from, to):
 
 	connections_from = _connections.get(to)
 	if connections_from:
-		var inv_connection = connections_from.get(from)
+		var inv_connection: Variant = connections_from.get(from)
 		if inv_connection:
 			inv_connection.offset = 0
 			inv_connection.join()
 	return connection.line
 
-# Clear all selection
-func clear_connections():
+
+func clear_connections() -> void:
 	for connections_from in _connections.values():
 		for connection in connections_from.values():
 			connection.line.queue_free()
 	_connections.clear()
-			
-# Return array of dictionary of connection as such [{"from1": "to1"}, {"from2": "to2"}]
-func get_connection_list():
-	var connection_list = []
+
+
+func get_connection_list() -> Array:
+	var connection_list: Array = []
 	for connections_from in _connections.values():
 		for connection in connections_from.values():
 			connection_list.append({"from": connection.from_node.name, "to": connection.to_node.name})
 	return connection_list
 
-class Connection:
-	var line # Control node that draw line
-	var from_node
-	var to_node
-	var offset = 0 # line's y offset to make space for two interconnecting lines
 
-	func _init(p_line, p_from_node, p_to_node):
+class Connection:
+	var line: Control
+	var from_node: Node
+	var to_node: Node
+	var offset: int = 0
+
+
+	func _init(p_line: Control, p_from_node: Node, p_to_node: Node) -> void:
 		line = p_line
 		from_node = p_from_node
 		to_node = p_to_node
 
-	# Update line position
-	func join():
+
+	func join() -> void:
 		line.join(get_from_pos(), get_to_pos(), offset, [from_node.get_rect() if from_node else Rect2(), to_node.get_rect() if to_node else Rect2()])
 
-	# Return start position of line
-	func get_from_pos():
+
+	func get_from_pos() -> Vector2:
 		return from_node.position + from_node.size / 2
 
-	# Return destination position of line
-	func get_to_pos():
+
+	func get_to_pos() -> Vector2:
 		return to_node.position + to_node.size / 2 if to_node else line.position
